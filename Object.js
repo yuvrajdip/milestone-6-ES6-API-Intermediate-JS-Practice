deec8b9c23f0c3a86a8ae0bd4c883e31f4b938dc
yuvrajdip <dipbhowmik2018@gmail.com>
2023-08-27 22:41:41 +0600

    first commit

---
+++ Object.js
@@ -0,0 +1,12 @@
+
+const student ={
+  name : "Deba Dip Bhowmik"
+}
+
+student.roll = 1601062
+
+console.log(student); // { name: 'Deba Dip Bhowmik', roll: 1601062 }
+
+delete student.roll
+
+console.log(student); // { name: 'Deba Dip Bhowmik' }
