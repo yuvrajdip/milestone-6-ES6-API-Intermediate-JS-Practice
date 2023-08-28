7455251e2bdd5f9c0fb0ce0779fd8e36cff001b2
yuvrajdip <dipbhowmik2018@gmail.com>
2023-08-28 15:31:16 +0600

    forEach

--- forEach.js
+++ forEach.js
@@ -1,5 +1,12 @@
 
-// forEach doesn't return 
+//* forEach doesn't return 
 const numbers = [1,5,6,4,15]
 
 numbers.forEach(element=>console.log(element))
+
+console.log(`\n`);
+numbers.forEach( number => {
+  if( number%2===0 )console.log(number)
+})
+
+// forEach modifies the input array
